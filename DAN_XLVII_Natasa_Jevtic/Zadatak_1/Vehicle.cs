4d42ed1c18c239f70d0aeb403c769da2e645bc48
natasajevtic <nataly.jevtic.95@gmail.com>
2020-07-16 17:23:07 +0200

    Class Vehicle created

---
+++ DAN_XLVII_Natasa_Jevtic/Zadatak_1/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Zadatak_1
+{
+    class Vehicle
+    {
+        public string Direction { get; set; }
+        public int Number { get; set; }
+        string[] directions = { "south", "north" };
+        static Random random = new Random();
+
+        public Vehicle(int number)
+        {
+            Number = number;
+            Direction = directions[random.Next(0, directions.Length)];
+        }
+    }
+}
