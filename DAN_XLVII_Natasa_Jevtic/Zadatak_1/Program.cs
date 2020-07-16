95e92cabcc6d5b8185a01684a75a2d801ffa94b2
natasajevtic <nataly.jevtic.95@gmail.com>
2020-07-16 17:59:25 +0200

    Methods for simulating crossing the bridge implemented

--- DAN_XLVII_Natasa_Jevtic/Zadatak_1/Program.cs
+++ DAN_XLVII_Natasa_Jevtic/Zadatak_1/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Zadatak_1
 {
@@ -12,6 +9,8 @@ namespace Zadatak_1
         static Vehicle[] vehicles;
         //delegate for notification
         delegate void Notification();
+        //declaring array of threads
+        public static Thread[] threadsForVehicle;
         /// <summary>
         /// This method initializes an array of vehicles, creates objects of class Vehicle, and fills the array with them.
         /// </summary>
@@ -36,7 +35,6 @@ namespace Zadatak_1
                 Console.WriteLine("{0}. vehicle - direction: {1}", vehicle.Number, vehicle.Direction);
             }
         }
-
         static void Main(string[] args)
         {
             CreateVehicle();
@@ -44,6 +42,22 @@ namespace Zadatak_1
             Notification notification = Notify;
             //invoking method Notify
             notification();
+            threadsForVehicle = new Thread[vehicles.Length];
+            //creating threads for crossing the bridge of every vehicle
+            for (int i = 0; i < threadsForVehicle.Length; i++)
+            {
+                threadsForVehicle[i] = new Thread(vehicles[i].CanCrossTheBridge)
+                { Name = vehicles[i].Direction };
+            }
+            //starting threads
+            for (int i = 0; i < threadsForVehicle.Length; i++)
+            {
+                threadsForVehicle[i].Start();
+            }
+            for (int i = 0; i < threadsForVehicle.Length; i++)
+            {
+                threadsForVehicle[i].Join();
+            }
         }
     }
 }
--- DAN_XLVII_Natasa_Jevtic/Zadatak_1/Vehicle.cs
+++ DAN_XLVII_Natasa_Jevtic/Zadatak_1/Vehicle.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using System.Linq;
 
 namespace Zadatak_1
 {
@@ -8,11 +10,66 @@ namespace Zadatak_1
         public int Number { get; set; }
         string[] directions = { "south", "north" };
         static Random random = new Random();
+        static string defaultDirection;
+        static EventWaitHandle canCross = new ManualResetEvent(false);
+        static object locker = new object();
+        bool lockTaken = false;
 
         public Vehicle(int number)
         {
             Number = number;
             Direction = directions[random.Next(0, directions.Length)];
+        }
+        /// <summary>
+        /// This method manages when the CrossTheBridge method can be invoked.
+        /// </summary>
+        public void CanCrossTheBridge()
+        {
+            try
+            {
+                Monitor.TryEnter(locker, ref lockTaken);
+                if (lockTaken)
+                {
+                    defaultDirection = Thread.CurrentThread.Name;
+                    CrossTheBridge();
+                }
+                else
+                {
+                    canCross.WaitOne();
+                    //if vehicle direction different from default direction, waiting for vehicles with default direction to cross the bridge
+                    if (Direction != defaultDirection)
+                    {
+                        Console.WriteLine("{0}. vehicle is waiting for the lane to clear to cross the bridge.", Number);
+                        var threads = Program.threadsForVehicle.Where(x => x.Name == defaultDirection);
+                        foreach (var item in threads)
+                        {
+                            item.Join();
+                        }
+                        CrossTheBridge();
+                    }
+                    else
+                    {
+                        CrossTheBridge();
+                    }
+                }
+            }
+            finally
+            {
+                // ensure that the lock is released
+                if (lockTaken)
+                {
+                    Monitor.Exit(locker);
+                }
+            }
+        }
+        /// <summary>
+        /// This method simulates crossing the bridge of the vehicle.
+        /// </summary>
+        public void CrossTheBridge()
+        {
+            Thread.Sleep(500);
+            Console.WriteLine("{0}. vehicle has crossed the bridge in the direction of the {1}.", Number, Direction);
+            canCross.Set();
         }
     }
 }
